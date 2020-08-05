3ae55d00a3691399390247d2aa1202b310a47e9a
Eve Washington <eve.suane@gmail.com>
2020-08-05 12:34:25 -0400

    fix typo

--- leaflet.R
+++ leaflet.R
@@ -3,18 +3,32 @@ library(leaflet)
 library(htmltools)
 
 m <- leaflet() %>%
-  setView(lng = -83.148, lat = 42.2827, zoom = 13) %>%
+  setView(lng = -83.097113, lat= 42.2876, zoom = 13) %>%
   addTiles()
 
 #add 48217 to the leaflet
-# created geojson with https://mapshaper.org/
+# created geojson with https://mapshaper.org/     ; direction = 'top',
 
 bondries <- readLines("Data/Detroit_Zip_Codes_filtered.json") %>% paste(collapse = "\n")
-m <- m %>% addGeoJSON(bondries)
+m <- m %>% addGeoJSON(bondries) %>% addLabelOnlyMarkers(lat=42.2827, lng=-83.1488, label = "48217",
+                                                        labelOptions = labelOptions(noHide = TRUE,
+                                                                                    direction = 'bottom', 
+                                                                                    textOnly = TRUE, 
+                                                                                    textsize = "30px", 
+                                                                                    style = list(
+                                                                                      "color" ='blue'))
+                                                        )
+
 
 bondries_2 <- readLines("Data/Detroit_Zip_Codes_48209.json") %>% paste(collapse = "\n")
-m <- m %>% addGeoJSON(bondries_2, color = 'yellow')
-
+m <- m %>% addGeoJSON(bondries_2, color = 'yellow',)%>% addLabelOnlyMarkers(lat=42.3054, lng=-83.1166, label = "48209",
+                                                                            labelOptions = labelOptions(noHide = TRUE,
+                                                                                                        direction = 'top', 
+                                                                                                        textOnly = TRUE, 
+                                                                                                        textsize = "30px", 
+                                                                                                        style = list(
+                                                                                                          "color" ='#d1aa28'))
+                                                                            )
 
 #set icons
 #tri_icon = makeIcon("img/warning.png", "img/warning2x.png", 18, 18)
@@ -39,8 +53,8 @@ monitoring_icon = awesomeIcons(icon = 'eye',
 #add TRI points to leaflet
 
 html_leg <- " <p style='color:black; font-weight: bold;'>Marathon Monitoring Locations</p>
-              <p style='color:green; font-weight: bold;'>EGLE Monitoring Sites Discueesed</p>
-              <p style='color:red; font-weight:bold;'>Industry Sites Discueesed</p> "
+              <p style='color:green; font-weight: bold;'>EGLE Monitoring Sites Discussed</p>
+              <p style='color:red; font-weight:bold;'>Industry Sites Discussed</p> "
 
 m <- m %>% addAwesomeMarkers(data = TRI_2018_MI_filtered, icon = tri_icon, label = ~htmlEscape(NAME) )
 
